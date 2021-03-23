521a66692c3bb241d378e1be4a8affd751ba1c49
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-03-23 15:50:09 +0100

    JS: dropdown funzionanti con scomparsa

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
+// Funzione comparsa dropdown
+function dropAppear(){
+    // Controllo se ci sono altri drop attivi e li rimuovo
+    var links = $('.lista-nav > li');
+    // Richiamo la funzione disappear per rimuoverli
+    disappear(links);
 
+    // Attivo il drop corrente
+    var link = $(this);
+    var dropdown = link.children('div.dropdown');
+    dropdown.addClass('active');
+}
 
+// Funzione scomparsa
+function disappear(links) {
+    links.each(function () {
+        links.children('div.dropdown').removeClass('active');
+    });
+}
+
+// Funzione scomparsa se clicco fuori
+function dropDisappear() {
+    $(window).click(function () {
+        var links = $('.lista-nav > li');
+        disappear(links);
+    })
+}
 
 
 // Funzione iniziale
 function init(){
-    
+    $('.lista-nav > li').mouseenter(dropAppear);
+    $('.lista-nav > li').mouseout(dropDisappear);
 }
 
 $(document).ready(init);
